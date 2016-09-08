7ec3804b70468834a2c81dd1025137b346ccae17
Metheus101 <gutekunstmarkus@gmx.net>
2016-09-08 14:43:03 +0200

    Init

---
+++ PingIt.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		PingIt.ino
+ Created:	08.09.2016 14:40:44
+ Author:	gutek
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
